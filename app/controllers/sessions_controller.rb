771240bb02e0b443f20a02f105d0aca447903803
Jonas Oggier <jonas.oggier@gmail.com>
2013-04-15 17:22:45 -0400

    Implementing basic user registration and sessions with Sorcery.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+
+	# Login page
+  def new
+  end
+
+  # Login submit action
+  def create
+		@user = login(params[:email], params[:password])
+		if @user
+			redirect_to :root, :notice => "Welcome back, #{current_user.first_name}!"
+		else
+			flash.now[:alert] = "Invalid credentials. Try again!"
+			render :new
+		end
+  end
+
+  # Logout submit
+  def destroy
+		logout
+		redirect_to :root, notice: "Bye... Hope to see you later!"
+  end
+
+end
---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ActiveRecord::Base
+  authenticates_with_sorcery!
+
+  attr_accessible :first_name, :last_name, :email, :teaser, :description, :password, :password_confirmation
+
+  validates_length_of :password, :minimum => 5, :message => "Password must be at least 5 characters long", :if => :password
+  validates_confirmation_of :password, :message => "Password should match confirmation", :if => :password
+  validates :email, :presence => true, :uniqueness => true
+  
+end
