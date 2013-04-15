64c870375eb422427f517accc8b405dfc8653b1b
Bitmaker <Bitmaker@Bitmaker08.local>
2013-04-15 16:24:42 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lawyerly::Application.config.secret_token = 'ffb9d575bfc1323e173d789e2f9a928bfb496a9fed341134db6dec843fa0c1086ae4a1ba55b3caf7d74eff73308e4c212928afd3f7e6482d6adad232cd4e52b5'
