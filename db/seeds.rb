3d1b403efb5d35043546cd37aec33f4e51489cf9
Jonas Oggier <jonas.oggier@gmail.com>
2013-04-16 16:37:27 -0400

    Initializing seed file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,28 +1,31 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
 
+namecounter = 1
 
-week_counter = 1
-
-
-week_array = ["week1", "week2", "week3", "week4", "week5", "week6", "week7", "week8", "week9"]
-day_array = ["day1", "day2", "day3", "day4", "day5"]
-
-9.times do 
-	week_name = Week.create(name: week_array[week_counter-1], week_number: week_counter)
-	day_counter = 1
- 	5.times do
- 		week_name.days.create(day_of_week: day_counter) 
- 		day_counter+= 1
+50.times do 
+	user = User.create(first_name: first_names[namecounter-1], last_name: last_names[namecounter-1], email: '#{first_names[counter-1]}.#{last_names[counter-1]}@gmail.com', teaser: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua.', description: 'Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua.', password: 'testttt')
+ 	15.times do
+ 		user.posts.create(title: 'Lorem ipsum dolor sit amet', body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum.') 
  	end
- 	week_counter += 1
+ 	namecounter += 1
 end
 
+500.times do 
+	relationship = Relationship.create(user_id: Random.rand(25), followed_user_id: (Random.rand(50)-25))
+end
 
 last_names = %w[
 Malcom
