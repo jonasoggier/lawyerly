4bed59e60829f889325a5fae51e574ce81054bd4
Jonas Oggier <jonas.oggier@gmail.com>
2013-04-17 15:20:54 -0400

    First stab at Ajax.

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,19 +2,20 @@ class RelationshipsController < ApplicationController
   
   def create
 		@relationship = current_user.relationships.build(:followed_user_id => params[:followed_user_id])
-	  if @relationship.save
-	    flash[:notice] = "You are now following xxx ."
-	    redirect_to current_user
-	  else
-	    flash[:notice] = "Unable to follow xxx."
-	    redirect_to current_user
-	  end
+	  @relationship.save
+
+	  respond_to do |format|
+      format.html { redirect_to current_user }
+      format.js
+    end
 	end
 
   def destroy
 	  @relationship = current_user.relationships.find(params[:id])
 	  @relationship.destroy
-	  flash[:notice] = "You have succesfully unfollowed xxx."
-	  redirect_to current_user
+	  respond_to do |format|
+      format.html { redirect_to current_user }
+      format.js
+    end
 	end
 end
