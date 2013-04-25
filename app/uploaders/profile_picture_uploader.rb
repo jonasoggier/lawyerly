57d7346573c79c8e68de0a0245ffc8501754fe4a
Jonas Oggier <jonas.oggier@gmail.com>
2013-04-25 14:59:15 -0400

    Adding image resizing upon upload.

--- app/uploaders/profile_picture_uploader.rb
+++ app/uploaders/profile_picture_uploader.rb
@@ -27,10 +27,10 @@ class ProfilePictureUploader < CarrierWave::Uploader::Base
   end
 
   # Process files as they are uploaded:
-  # process :scale => [500, 333]
+  process :resize_to_fill => [600, 450]
   #
   # def scale(width, height)
-  #   # do something
+  #   process :resize_to_fill => [width, height]
   # end
 
   # Create different versions of your uploaded files:
