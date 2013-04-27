05c33a3a51c73e527e54bfb59c2c4c2c8a7becc0
Jonas Oggier <jonas.oggier@gmail.com>
2013-04-27 14:08:35 -0400

    Finishing Notifications.

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,9 +2,16 @@ class NotificationsController < ApplicationController
 
   def destroy
     @notification = Notification.find(params[:id])
-    @user = User.find(@notification.follower_id) # needed for redirect
-    @notification.destroy
-    redirect_to @user
-  end
 
+    if @notification.follower_id != nil
+    	@user = User.find(@notification.follower_id)  # needed for redirect
+   		@notification.destroy
+			redirect_to @user
+    else
+     	@post = Post.find(@notification.post_id) # needed for redirect
+			@notification.destroy
+    	redirect_to user_post_path(current_user, @post)
+    end
+    
+  end
 end
--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,5 +1,5 @@
 class Notification < ActiveRecord::Base
   belongs_to :user
-  attr_accessible :user_id, :follower_id, :read
+  attr_accessible :user_id, :follower_id, :commenter_id, :post_id
 
 end
